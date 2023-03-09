fac7d92c3d663d6271a813917a5fcbb0b4e89564
proyectofinalcurriculovitae <127364914+proyectofinalcurriculovitae@users.noreply.github.com>
2023-03-08 23:10:39 -0300

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,30 @@
+document.getElementById('botón').addEventListener('click',function(){
+  document.getElementById('info').style.display = 'block';
+})
+
+document.getElementById('ocultar').addEventListener('click',function(){
+  document.getElementById('info').style.display = 'none';
+})
+
+document.getElementById('botón1').addEventListener('click',function(){
+  document.getElementById('dest').style.display = 'block';
+})
+
+document.getElementById('ocultar1').addEventListener('click',function(){
+  document.getElementById('dest').style.display = 'none';
+})
+
+document.getElementById('botón2').addEventListener('click',function(){
+  document.getElementById('expe').style.display = 'block';
+})
+
+document.getElementById('ocultar2').addEventListener('click',function(){
+  document.getElementById('expe').style.display = 'none';
+})
+document.getElementById('botón3').addEventListener('click',function(){
+  document.getElementById('form').style.display = 'block';
+})
+
+document.getElementById('ocultar3').addEventListener('click',function(){
+  document.getElementById('form').style.display = 'none';
+})
